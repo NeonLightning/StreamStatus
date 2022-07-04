f3e80339ef25118f6dac9cda7bf3789b1cbbb856
NeonLightning <neonlightning@gmail.com>
2022-07-03 21:03:19 -0400

    update

--- frontsrc/StreamStatusNG/Modlistform.vb
+++ frontsrc/StreamStatusNG/Modlistform.vb
@@ -1,30 +1,28 @@
-﻿Imports System.Xml
+﻿Imports System.Runtime.InteropServices.ComTypes
+Imports System.Xml
 
 Public Class ModlistForm
     Public modlistentry As New List(Of String)
-
-    Private Sub ModlistForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles AddModButton.Click
+    Public Sub Modlist_update()
         Dim xmlset As XmlWriterSettings = New XmlWriterSettings()
         xmlset.Indent = True
         xmlset.NewLineOnAttributes = True
+        Using writer2 As XmlWriter = XmlWriter.Create(".\modlist.xml", xmlset)
+            writer2.WriteStartDocument()
+            For Each modnamed In modlistentry
+                writer2.WriteStartElement("listomods")
+                writer2.WriteElementString("modname", modnamed)
+                Me.MainModList.Items.Add(modnamed)
+                writer2.WriteEndElement()
+            Next
+            writer2.WriteEndDocument()
+        End Using
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles AddModButton.Click
         If Not String.IsNullOrWhiteSpace(ModTextBox.Text) Then
             modlistentry.Add(ModTextBox.Text)
-            MainModList.Items.Clear()
-            Using writer2 As XmlWriter = XmlWriter.Create(".\modlist.xml", xmlset)
-                writer2.WriteStartDocument()
-                writer2.WriteStartElement("listomods")
-                For Each myMod In Me.modlistentry
-                    writer2.WriteElementString("modname", myMod)
-                Next
-                writer2.WriteEndElement()
-                writer2.WriteEndDocument()
-            End Using
-            For Each listmain In modlistentry
-                Me.MainModList.Items.Add(listmain)
-            Next
+            Me.ModTextBox.Clear()
+            Modlist_update()
             Me.ModTextBox.Text = ""
         End If
     End Sub
